15fd8404fe0babbca5aec31fb1631d9bf2c16d0f
cloudwolf <206946888+xerxes324@users.noreply.github.com.>
2025-12-19 14:01:35 +0530

    servers for loadbalancing

---
+++ proxy/loadbalancer/backends.py
@@ -0,0 +1,5 @@
+BACKENDS = {
+    "b1" : {"url" : "http://localhost:5000", "alive": True},
+    "b2" : {"url" : "http://localhost:5001", "alive": True},
+    "b3" : {"url" : "http://localhost:5002", "alive": True},
+}
